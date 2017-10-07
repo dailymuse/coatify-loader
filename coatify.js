eb5954080de6ad26236a663523b481da08cfca3a
Ian Axelrod <ian@themuse.com>
2017-10-06 21:14:39 -0400

    Make cacheable

--- coatify.js
+++ coatify.js
@@ -2,6 +2,7 @@ var coat = require("mithril-coat");
 
 function coatify(source) {
     var result;
+    this.cacheable && this.cacheable();
     coat.compile(source, { wrapFunction: true }, function(error, compiledTemplate) {
         if (error) {
             throw new Error("WUTEVA I DO WHAT I WANT");
